103ba0af7caee717096e51c9e0eb3402d48208be
Deigon Prates <deigon_prates@hotmail.com>
2018-01-18 21:37:38 -0300

    excluido funcoes desnecessarias

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -16,6 +16,6 @@ class Login extends CI_Controller {
     
     public function index() {
         $this->load->helper('form');
-        $this->load->view('login');
+        $this->load->view('inicio');
     }
 }
